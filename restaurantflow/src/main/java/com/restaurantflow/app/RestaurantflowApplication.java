b7caa9d5fcd3095bee57cce2ff07706008d10303
Harshagithu <gottipatiharsha21@gmail.com>
2026-06-24 22:42:52 +0530

    First Commit: project setup — Spring Boot 3.5.15, MySQL, JSP config, pom.xml

---
+++ restaurantflow/src/main/java/com/restaurantflow/app/RestaurantflowApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantflow.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantflowApplication.class, args);
+	}
+
+}
---
+++ restaurantflow/src/test/java/com/restaurantflow/app/RestaurantflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantflow.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
